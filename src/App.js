1747ed6021fb3251b5e8668a797cc4b7d2afc42c
Ariane Ueti <arianeueti@gmail.com>
2021-09-17 22:50:34 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
         <Route path="/favorites" component={ Favorites } />
         <Route path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="" component={ NotFound } />
+        <Route path="*" component={ NotFound } />
       </BrowserRouter>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+
+    this.setState({
+      name: value,
+    });
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name });
+    // Início Referência: Consultei o repos. https://github.com/tryber/sd-014-b-project-trybetunes/pull/4/ para organizar a lógica
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
+  }
+
   render() {
+    const { name, loading, redirect } = this.state;
+    const THREE = 3;
+    if (loading) return <Loading />;
+    if (redirect) return <Redirect to="/search" />;
+    // Fim referência
     return (
       <div data-testid="page-login">
         <h1> Login </h1>
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ name.length < THREE }
+            value="Entrar"
+            onClick={ this.handleSubmit }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
