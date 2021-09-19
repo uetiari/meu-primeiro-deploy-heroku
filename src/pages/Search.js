f3b1767dee5f17bcbbc9ee6bb2db2e4bc7288245
Ariane Ueti <arianeueti@gmail.com>
2021-09-18 23:28:19 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
+    const TWO = 2;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="search"
+            value={ search }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            value="pesquisar"
+            data-testid="search-artist-button"
+            disabled={ search.length < TWO }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
