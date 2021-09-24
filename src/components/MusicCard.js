73efc2c1292a3e2aec67d4e4c65ece5e4f8d2c14
Ariane Ueti <arianeueti@gmail.com>
2021-09-23 23:27:45 -0300

    Requisito 8 - Parte 2 e 3

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Loading from '../pages/Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+  favoriteFunc = async () => {
+    const { music } = this.props;
+    this.setState({
+      loading: true,
+    });
+    await addSong(music);
+
+    this.setState({
+      loading: false,
+    });
+  }
+
   render() {
     const { previewUrl, name, trackId } = this.props;
+    const { loading } = this.state;
+    if (loading) return <Loading />;
+
     return (
       <div>
         <p>{ name }</p>
@@ -17,6 +42,7 @@ class MusicCard extends React.Component {
           <input
             type="checkbox"
             data-testid={ `checkbox-music-${trackId}` }
+            onChange={ this.favoriteFunc }
           />
           Favorita
         </label>
@@ -29,6 +55,7 @@ MusicCard.propTypes = {
   previewUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   trackId: PropTypes.number.isRequired,
+  music: PropTypes.string.isRequired,
 };
 
 export default MusicCard;
